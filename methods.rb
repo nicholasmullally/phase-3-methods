3cc674e8fe63c060ee92aff28e7d3ef41d65a14b
Nicholas <mullally.nt@gmail.com>
2022-07-14 13:18:26 -0700

    Done with assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+
+# 1.
+def greet_programmer
+    puts "Hello, programmer"
+end
+
+# 2. 
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# 3.
+
+def greet_with_default(name)
+    if name = #{}
+        puts "Hello, programmer!"
+    elsif puts "Hello, #{name}!"
+
+# 4. 
+
+def add(x,y)
+    x + y
+end
+
+# 5.
+def halve(num)
+    if num === int
+        num / 2
+    elsif return nil
+    end
+    
+    
+
+
